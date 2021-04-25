342a1d075c2a3e75ccbbc96747788080b2a64377
Veress Gábor <veressgabi33@gmail.com>
2021-04-25 10:09:35 +0200

    Main class implemented

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -1,9 +1,36 @@
 package user;
 
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import java.util.List;
+import java.util.Locale;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+        FakeUserGenerator userGenerator = new FakeUserGenerator(new Locale("hu"));
+        List<User> users = jdbi.withExtension(UserDAO.class, dao ->
+        {
+            dao.createTable();
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
+            System.out.println(dao.addUser(userGenerator .createUser()));
 
+            System.out.println(dao.getUserById((long)2));
+            System.out.println(dao.getUserByUsername(dao.getUserById((long)2).get().getUsername()));
+            dao.deleteUser(dao.getUserById((long)2).get());
+
+            return  dao.listUsers();
+        });
+        users.forEach(System.out::println);
     }
 }
